027d16a1c21801ff5aeb2672179fbdc2b9c84bc8
Pradish Bijukchhe <pradishbijukchhe@gmail.com>
2025-07-03 01:56:10 +0545

    fix: fix pre-commit

--- django_admin_select_columns/apps.py
+++ django_admin_select_columns/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class DjangoAdminSelectColumnsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'django_admin_select_columns'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "django_admin_select_columns"
